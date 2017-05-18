80fb2e1fec4380535ac9738d9d025b7a87fbd82a
Shinmera <shinmera@tymoon.eu>
2017-05-18 16:02:22 +0200

    Start working on graph stuff I guess.

--- graph.lisp
+++ graph.lisp
@@ -6,8 +6,37 @@
 
 (in-package #:org.shirakumo.flow)
 
-(defgeneric visit (root-node function))
-(defgeneric extract-graph (root-node))
+(defun visit (node function)
+  (let ((visited (make-hash-table :test 'eq)))
+    (labels ((%visit (node)
+               (unless (gethash node visited)
+                 (setf (gethash node visited) node)
+                 (funcall function node)
+                 (dolist (connection (connections node))
+                   (cond ((eql node (left connection))
+                          (%visit (right connection)))
+                         ((eql node (right connection))
+                          (%visit (left connection))))))))
+      (%visit node))))
+
+(defun extract-graph (node)
+  (let ((vertices ())
+        (edges ()))
+    (flet ((connect (left right)
+             (pushnew (list left right) edges :test #'equal)))
+      (visit node (lambda (node)
+                    (push node vertices)
+                    (dolist (connection (connections node))
+                      (etypecase connection
+                        (directed-connection
+                         (connect (left connection) (right connection)))
+                        (connection
+                         (connect (left connection) (right connection))
+                         (connect (right connection) (left connection)))))))
+      (values vertices edges))))
+
 (defun topological-sort (root-node))
+
 (defun color-nodes (root-node))
+
 (defun color-outputs (root-node))
