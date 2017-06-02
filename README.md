e96be38c20574fe01c3ca3482f1d957416adedd3
Shinmera <shinmera@tymoon.eu>
2017-06-02 16:16:09 +0200

    Documentation

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-Don't look, AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa
+## About Flow
+Flow is a flowchart graph library. Unlike other graphing libraries, this one focuses on nodes in a graph having distinct "ports" through which connections to other nodes are formed. This helps in many concrete scenarios where it is important to distinguish not only which nodes are connected, but also how they are connected to each other.
+
+Particularly, a lot of data flow and exchange problems can be reduced to such a "flowchart". For example, an audio processing library may present its pipeline as a flowchart of segments that communicate with each other through audio sample buffers. Flow gives a convenient view onto this kind of problem, and even allows the generic visualisation of graphs in this format.
+
+## How To
+In a Flow graph there's three kinds of `unit`s: `node`s, `port`s, and `connection`s. A node is analogous to a vertex in a graph, a port is analogous to a place where a connection can be made on a node, and a connection is analogous to an edge in a graph.
+
+Of the nodes, there's two kinds: 
+
+* `dynamic-node` A dynamic node's ports are determined at runtime for each individual instance. This is useful for when you're constructing your graph out of elements that you don't know ahead of time.
+* `static-node` A static node's ports are determined at class definition time, and each port corresponds to a special kind of slot on the graph. This is usually what you want when you define your graph entirely yourself.
+
+Of the ports, there's several mixin classes that can be used to define the kind of port you want. Naturally, if you want to add extra information you can define your own port classes to use instead.
+
+* `n-port` A port that accepts an arbitrary number of connections.
+* `1-port` A port that only accepts a single connection.
+* `in-port` A port that only accepts incoming connections.
+* `out-port` A port that only accepts outgoing connections.
+
+Of the connections, only two are predefined, though it is easy to imagine situations where other kinds of connections might also come in handy.
+
+* `connection` A basic undirected connection that goes both ways.
+* `directed-connection` A directed connection that only goes from left to right.
+
+You can then manage connections between ports using `connect`, `disconnect`, and `sever`. You can also inspect nodes and ports with `ports`, and `connections`.
+
+### A Flow Chart Example
+If you wanted to build a classic flow chart library, you could use something like this as your basic building blocks:
+
+    (defclass in (in-port n-port)
+      ())
+    
+    (defclass out (out-port 1-port)
+      ())
+    
+    (define-node start ()
+      ((out :port-type out)))
+    
+    (define-node end ()
+      ((in :port-type in)))
+    
+    (define-node process ()
+      ((in :port-type in)
+       (out :port-type out)))
+    
+    (define-node decision ()
+      ((in :port-type in)
+       (true :port-type out)
+       (false :port-type out)))
+
+Using these basic classes we can then create a flow chart like this:
+
+    (let ((start (make-instance 'start))
+          (pick-library (make-instance 'process))
+          (evaluate-library (make-instance 'process))
+          (decide-if-good (make-instance 'decision))
+          (end (make-instance 'end)))
+      (connect (port start 'out) (port pick-library 'in))
+      (connect (port pick-library 'out) (port evaluate-library 'in))
+      (connect (port evaluate-library 'out) (port decide-if-good 'in))
+      (connect (port decide-if-good 'true) (port end 'in))
+      (connect (port decide-if-good 'false) (port pick-library 'in))
+      start)
+
+### Operating on Flow Graphs
+Flow also includes a couple of operations to help your process the graphs you created using the library. It can do a `topological-sort`, `extract-graph` for you, `color-nodes`, and `allocate-ports`. There's also a generic `visit` to allow you to quickly traverse the graph. See the docstrings of the functions for an in-depth explanation of what they do.
+
+### Visualising a Flow Graph
+There is an additional system included called `flow-visualizer`. This system includes a primitive graph visualizer that lets you view and edit a graph directly in a GUI. It isn't very advanced at this point, but will probably be extended in the future to a usable flowchart editor.
--- documentation.lisp
+++ documentation.lisp
@@ -6,3 +6,428 @@
 
 (in-package #:org.shirakumo.flow)
 
+;; graph.lisp
+(docs:define-docs
+  (function visit
+    "Visit each node in the graph, starting from the given node.
+
+The visiting proceeds by calling the function on a
+node, then recursing through each connection of the
+node. The recursion does not respect directed
+connections. It is guaranteed that each node is
+only visited once, regardless of cycles.")
+  
+  (function extract-graph
+    "Extract the graph starting from the given node.
+
+This returns two lists, the first being the list of
+vertices (nodes), and the second being the list of
+edges, with each edge being a list of left and right
+vertex that are connected. The edges are intended to
+be directed. Undirected edges are represented by
+two edges, one from left to right and one from right
+to left.
+
+The order of the vertices and edges in the returned
+lists is unspecified.
+
+See VISIT")
+  
+  (function topological-sort
+    "Produces a topological sorting of the given nodes.
+
+This uses Tarjan's algorithm to compute the
+topological sorting. Note that if the given list
+of nodes does not include all reachable nodes, the
+result may be erroneous.")
+  
+  (function color-nodes
+    "Perform a graph colouring.
+
+Each node in the graph from the given starting node
+out is assigned a \"colour\" to the specified
+attribute. This colour is in practise an integer in
+the range [0,n] where n is the number of nodes in
+the graph. The colours are distributed in such a way
+that no neighbouring nodes have the same colour.
+
+The employed algorithm is greedy and cannot guarantee
+an optimal colouring. Optimal colouring is an NP-
+complete problem, and the results produced by a
+greedy algorithm are usually shown to be good enough.
+
+The full list of coloured nodes is returned.")
+  
+  (function allocate-ports
+    "Perform a colour \"allocation\" on the ports of the graph.
+
+Each port reachable in the graph from the given
+starting nodes out that is not of type in-port is
+assigned a \"colour\" to the specified attribute.
+If clear is non-NIL, the colour attribute is first
+cleared off of each port, ensuring a clean colouring.
+
+The colouring rules are as follows:
+A port may not have the same colour as any of the
+other ports on the same node. Unless the node's
+in-place-attribute is non-NIL, the colour must
+also be distinct from the colour of any of the
+node's predecessor ports. A predecessor port being
+any port that is connected to an in-port of the
+node.
+
+In effect this produces a colouring that is useful
+to calculate the allocation of buffers and other
+resources necessary to perform a calculation for
+a node. These rules ensure that the calculation
+can be performed without accidentally overwriting
+buffer data necessary at a later point in the
+execution of the graph, while at the same time
+also minimising the number of necessary buffers.
+
+The given graph may not contain any cycles.
+
+Returned is a topologically sorted list of the
+inputted nodes that can be used as the execution
+order of the nodes in the graph.
+
+See TOPOLOGICAL-SORT"))
+
+;; nodes.lisp
+(docs:define-docs
+  (variable *resolve-port*
+    "Whether a slot-value/slot-makunbound/slot-boundp call should resolve the port.
+
+If this is T (the default), then the port's
+slot within the object's slot is resolved,
+rather than directly resolving the slot that the
+port is itself contained in.")
+  
+  (type unit
+    "Superclass for all any entity in a Flow graph.
+
+See ATTRIBUTES
+See ATTRIBUTE
+See REMOVE-ATTRIBUTE
+See WITH-ATTRIBUTES")
+  
+  (function attributes
+    "Accessor to the unit's hash table of attributes.
+
+See UNIT
+See ATTRIBUTE
+See REMOVE-ATTRIBUTE")
+  
+  (function attribute
+    "Accessor to the named attribute on the unit.
+
+The attribute's name must be comparable by EQL.
+If the attribute does not exist on the unit, the
+default value is returned instead.
+
+See ATTRIBUTES
+See REMOVE-ATTRIBUTE
+See UNIT")
+  
+  (function remove-attribute
+    "Remove the named attribute from the unit.
+
+See ATTRIBUTES
+See ATTRIBUTE
+See UNIT")
+  
+  (function with-attributes
+    "Shorthand macro to access the given attributes through a variable.
+
+This is similar to WITH-SLOTS.
+
+See UNIT
+See ATTRIBUTE
+See CL:WITH-SLOTS")
+  
+  (type connection
+    "Representation of a connection between two ports.
+
+This connection is undirected, meaning that it is
+intended to represent information flowing in both
+directions.
+
+See LEFT
+See RIGHT
+See UNIT
+See CONNECTION=
+See SEVER")
+  
+  (function left
+    "Accessor to the \"left\" port of a connection.
+
+See CONNECTION")
+  
+  (function right
+    "Accessor to the \"right\" port of a connection.
+
+See CONNECTION")
+  
+  (function connection=
+    "Tests whether two connections are considered equal.
+
+Connections are the same under this comparison, if
+they are connected to the same ports \"in the same
+way\". This simply means that whether ports are
+connected the same may depend on the specific
+connection being tested. For example, directed
+connections are only the same if the left and right
+ports match up, whereas undirected connections are
+the same regardless of the order between them.
+
+See CONNECTION")
+  
+  (function sever
+    "Sever the connections of this unit.
+
+For a connection, severing it means simply removing
+that connection. For a port severing means severing
+all connections of the port. For a node severing
+severing all connections of all of its ports.
+
+See CONNECTION
+See PORT
+See NODE")
+  
+  (type directed-connection
+    "A connection for which information only flows from left to right.
+
+See CONNECTION")
+  
+  (type port
+    "Representation of a connection port on a node.
+
+Ports are named places on a node through which
+connections between nodes can be made.
+
+See UNIT
+See CONNECTIONS
+See NODE
+See SLOT
+See CONNECT
+See DISCONNECT
+See REMOVE-CONNECTION
+See CHECK-CONNECTION-ACCEPTED
+See SEVER")
+  
+  (function connections
+    "Accessor to the list of connections on this unit.
+
+The list is not guaranteed to be fresh and thus
+may not be modified without potentially messing things
+up.
+
+See PORT
+See NODE")
+  
+  (function node
+    "Accessor to the node this port is home to.
+
+See PORT")
+  
+  (function slot
+    "Accessor to the name of the slot this port is contained in.
+
+See PORT")
+  
+  (function connect
+    "Forge a connection between the two units.
+
+The connection is only made if it is accepted on both
+left and right hand sides by CHECK-CONNECTION-ACCEPTED.
+If both accept the connection, it is pushed onto their
+respective connections lists.
+
+See PORT
+See CHECK-CONNECTION-ACCEPTED
+See CONNECTIONS")
+  
+  (function disconnect
+    "Remove any matching connection from left to right.
+
+This constructs a directed-connection between the two
+and then removes all connections from each of them that
+matches the constructed connection by CONNECTION=.
+
+See PORT
+See DIRECTED-CONNECTION
+See REMOVE-CONNECTION
+See CONNECTION=")
+  
+  (function remove-connection
+    "Remove the given connection from the unit.
+
+See PORT
+See NODE
+See CONNECTIONS")
+  
+  (function check-connection-accepted
+    "Check whether the given connection is accepted on the given unit.
+
+If it is not accepted, an error is signalled. This
+generic function uses a PROGN method combination,
+which forces tests of all superclasses to be performed
+as well.")
+  
+  (type n-port
+    "A port that accepts an arbitrary number of connections.
+
+See PORT")
+  
+  (type 1-port
+    "A port that only accepts a single connection.
+
+See PORT")
+  
+  (type in-port
+    "A port that only accepts incoming connections.
+
+See PORT")
+  
+  (type out-port
+    "A port that only accepts outgoing connections.
+
+See PORT")
+  
+  (type node
+    "Superclass for all nodes in a Flow graph.
+
+A node has a set of PORT instances that are
+used to form connections to other nodes over.
+
+See UNIT
+See PORT
+See PORTS
+See SEVER
+See CONNECTIONS
+See REMOVE-CONNECTION
+See DISCONNECT")
+  
+  (function ports
+    "Returns a list of port objects that the node contains.
+
+This list may not be fresh and thus must not be modified.
+
+See NODE")
+  
+  (function port
+    "Return the port object contained in the node with the specified name.
+
+See NODE")
+
+  (type dynamic-node
+    "Superclass for all dynamic nodes.
+
+A dynamic node's ports are allocated on a per-instance
+basis, rather than on a per-class basis like for the
+static-node.
+
+See NODE"))
+
+;; static-node.lisp
+(docs:define-docs
+  (type port-definition
+    "Superclass for port definition slot classes.
+
+See PORT-TYPE")
+  
+  (function port-type
+    "Accessor to the port type contained in this slot.
+
+See PORT-DEFINITION")
+  
+  (type direct-port-definition
+    "Class for direct port slot definitions
+
+See PORT-DEFINITION
+See C2MOP:STANDARD-DIRECT-SLOT-DEFINITION")
+  
+  (type effective-port-definition
+    "Class for effective port slot definitions
+
+See PORT-DEFINITION
+See C2MOP:STANDARD-EFFECTIVE-SLOT-DEFINITION")
+  
+  (type static-node-class
+    "Metaclass for all static nodes.
+
+This class allows the usage of the :PORT-TYPE initarg
+on slots. If non-null, the slot is treated as a port
+of the node, allowing to be used for connections
+between nodes. When such a slot is accessed normally,
+it immediately resolves to the connections slot in the
+port contained in the slot.
+
+Every port of a port-typed slot is also automatically
+instantiated upon instantiation of the class itself,
+ensuring that it is consistent with the definition.
+
+If an access to the actual port object contained in the
+slot is necessary, the PORT-SLOT-VALUE and
+PORT-SLOT-BOUNDP functions can be used instead.
+
+See DIRECT-PORT-DEFINITION
+See EFFECTIVE-PORT-DEFINITION
+See DEFINE-NODE
+See PORT-SLOT-VALUE
+See PORT-SLOT-BOUNDP")
+  
+  (function port-slot-value
+    "Accessor to the actual port object contained in the node's slot.
+
+See STATIC-NODE
+See *RESOLVE-PORT*")
+  
+  (function port-slot-boundp
+    "Test to see whether the actual port object contained in the node's slot is bound.
+
+For any successfully initialised node, this should
+always return T.
+
+See STATIC-NODE
+See *RESOLVE-PORT*")
+  
+  (type static-node
+    "Superclass for all static nodes.
+
+The set of ports of a static node is defined per-class
+and is thus the same for each instance of the class.
+
+In addition to the standard slot keywords, a node
+supports the :PORT-TYPE keyword. This takes a symbol
+as argument, designating the name of the class to
+use for the port of this slot.
+
+If a slot is a port on the class, connections to
+other ports may be established through that port.
+
+See NODE
+See STATIC-NODE-CLASS
+See DEFINE-NODE
+See PORTS
+See PORT
+See SEVER
+See CONNECTIONS
+See REMOVE-CONNECTION
+See DISCONNECT")
+  
+  (function define-node
+    "Shorthand macro to define a static node class.
+
+All this does is add the necessary :METACLASS option
+and inject STATIC-NODE as a direct-superclass.
+
+See STATIC-NODE"))
+
+;; toolkit.lisp
+(docs:define-docs
+  (function find-slots-by-initarg
+    "Returns the list of slots that have key as an initarg.")
+  
+  (function find-slot-by-name
+    "Returns the slot whose name matches the given one."))
